72e37dd51ed29038895883999fac8d05292f4693
Anton Shcherbinin <anton.shcherbinin@stoloto.ru>
2023-06-01 03:32:10 +0300

    1st source commit

---
+++ task1/src/Main.java
@@ -0,0 +1,23 @@
+import static java.lang.System.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int arrayLenth,stepLenght;
+        arrayLenth = Integer.parseInt(args[0]);
+        stepLenght = Integer.parseInt(args[1]);
+
+        int currentPos = 1;
+        out.print(currentPos);
+        currentPos += stepLenght-1;
+        while (currentPos != 1) {
+            if(currentPos == 0)
+                out.print(arrayLenth);
+            else
+                out.print(currentPos);
+
+            currentPos += stepLenght-1;
+            if (currentPos >= arrayLenth)
+                currentPos -= arrayLenth;
+        }
+    }
+}
